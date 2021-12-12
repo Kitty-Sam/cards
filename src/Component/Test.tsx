7a7ac79a2bae171cdc86c7ca3d963cc6dd5d31ac
kitty-sam <www.moonlight@mail.ru>
2021-12-12 15:56:24 +0300

    changed 2dd

--- src/Component/Test.tsx
+++ src/Component/Test.tsx
@@ -11,7 +11,7 @@ const Test = () => {
             placeholder="enter smth?"/>
 
             <SuperButton>
-               press me again
+               press me againsss
             </SuperButton>
 
             <SuperCheckbox
